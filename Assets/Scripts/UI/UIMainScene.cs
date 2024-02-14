220a864bd981dfe01e4aa0627c5aebc341828686
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-14 16:49:17 +0200

    colors can now be saved and loaded from json file

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
